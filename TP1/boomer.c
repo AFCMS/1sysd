74e425ea35414ba2cb1b0c72b4f22c0cd9a06d67
AFCMS <afcm.contact@gmail.com>
2023-12-19 12:16:13 +0100

    TP1

---
+++ TP1/boomer.c
@@ -0,0 +1,17 @@
+//
+// Created by afcm on 19/12/23.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int i;
+    scanf("%d", &i);
+
+    if (i > 42) {
+        printf("Ok Boomer!\n");
+    } else {
+        printf("Time to learn UNIX\n");
+    }
+}
